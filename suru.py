7182dbf87eae786bf04894f6b9872bc67d6b4be8
Ryan <ssj4rydalton@gmail.com>
2026-01-06 15:47:24 -0500

    database accessing + read & write

--- suru.py
+++ suru.py
@@ -3,61 +3,97 @@ import requests
 from flask import Flask
 import sys
 import time
+import sqlite3
+import os
 
 print(sys.executable)
+if os.path.exists("suru.db"):
+	os.remove("suru.db")
+
+con = sqlite3.connect("suru.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE wishlist(link,name,price)")
+
+
+res = cur.execute("SELECT name FROM sqlite_master")
+res.fetchone()
+
+cur.execute("""
+	INSERT INTO wishlist VALUES
+            ('https://www.suruga-ya.com/en/product/ZHORE50831', 'Pedoshin', 0),
+            ('https://www.suruga-ya.com/en/product/ZHORE138875', 'エビゾメ 赫螺 Maru', 0)
+""")
+
+con.commit()
+res = cur.execute("SELECT link FROM wishlist")
+lis = res.fetchone()
+print(lis)
+
+res = cur.execute("SELECT name FROM wishlist")
+lis = res.fetchall()
+print(lis)
 
 surugayaPages = [
-	'https://www.suruga-ya.com/en/product/ZHORE50831',
-	'https://www.suruga-ya.com/en/product/ZHORE138875',
-	'https://www.suruga-ya.com/en/product/ZHORE70652',
-	'https://www.suruga-ya.com/en/product/ZHORE29719',
-	'https://www.suruga-ya.com/en/product/ZHORE79987',
-	'https://www.suruga-ya.com/en/product/ZHORE79987',
-	'https://www.suruga-ya.com/en/product/ZHORE80042',
-	'https://www.suruga-ya.com/en/product/ZHORE229720',
-	'https://www.suruga-ya.com/en/product/ZHORE138862',
-	'https://www.suruga-ya.com/en/product/ZHORE55361',
-	'https://www.suruga-ya.com/en/product/ZHORE9659',
-	'https://www.suruga-ya.com/en/product/186011708',
-	'https://www.suruga-ya.com/en/product/186023385',
-	'https://www.suruga-ya.com/en/product/186023384',
-	'https://www.suruga-ya.com/en/product/186147823',
-	'https://www.suruga-ya.com/en/product/186118307',
-	'https://www.suruga-ya.com/en/product/186114064',
-	'https://www.suruga-ya.com/en/product/186136845'
+	('https://www.suruga-ya.com/en/product/ZHORE50831', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE138875', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE70652', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE29719', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE79987', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE79987', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE80042', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE229720', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE138862', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE55361', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/ZHORE9659', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/186011708', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/186023385', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/186023384', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/186147823', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/186118307', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/186114064', 'BLANK', 0),
+	('https://www.suruga-ya.com/en/product/186136845', 'BLANK', 0)
 ]
 
-for x in surugayaPages:
-	time.sleep(3.0)
-	content = requests.get(x).text
-	#print(content)
+cur.executemany("INSERT INTO wishlist VALUES(?, ?, ?)", surugayaPages) #?'s are used as placeholder/substitutions
+con.commit() #transactions must be committed always after
 
-	soup = BeautifulSoup(content, "lxml")
-	addToCartBtn = soup.find("button",id='add-cart-btn')
-	if addToCartBtn is not None:
-		price = soup.find("input", class_="priceValue")
-		JPY = int(price["value"])
-		priceVal = f"¥{JPY:,.2f}"
-		priceVal = priceVal.rstrip("0").rstrip(".")
+for row in cur.execute("SELECT link, name FROM wishlist"): # TODO: do ORDER BY price whenever I get around to having page scrapes obtain that information
+	print(row)
 
 
-	print(addToCartBtn)
+con.close() #like C/C++ file closing, close database when done accessing
+
+# for x in surugayaPages:
+# 	time.sleep(3.0)
+# 	content = requests.get(x).text
+# 	#print(content)
+
+# 	soup = BeautifulSoup(content, "lxml")
+# 	addToCartBtn = soup.find("button",id='add-cart-btn')
+# 	if addToCartBtn is not None:
+# 		price = soup.find("input", class_="priceValue")
+# 		JPY = int(price["value"])
+# 		priceVal = f"¥{JPY:,.2f}"
+# 		priceVal = priceVal.rstrip("0").rstrip(".")
 
 
-	if addToCartBtn is not None:
-		print("PRODUCT AVAILABLE")
-		print(priceVal)
-	else:
-		print("PRODUCT UNAVAILABLE")
+# 	print(addToCartBtn)
+
+
+# 	if addToCartBtn is not None:
+# 		print("PRODUCT AVAILABLE")
+# 		print(priceVal)
+# 	else:
+# 		print("PRODUCT UNAVAILABLE")
 
 
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello world!"
+# @app.route("/")
+# def hello():
+# 	return "Hello world!"
 
 
 
