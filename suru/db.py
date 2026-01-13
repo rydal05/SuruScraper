91b4146a549c53f7a5909982f8fbeb128eae2acc
Ryan <ssj4rydalton@gmail.com>
2026-01-13 13:20:23 -0500

    blank

--- suru/db.py
+++ suru/db.py
@@ -1,14 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
-from flask import Flask
-import sys
-import time
-import sqlite3
-import os
 
-print(sys.executable)
-if os.path.exists("suru.db"):
-	os.remove("suru.db")
+import sqlite3
+from datetime import datetime
+
+import click
+from flask import current_app, g
+
+def get_db():
+	if 'db' not in g:
+		g.db = sqlite3.connect(
+			current_app.config['DATABASE'],
+			detect_types = sqlite3.PARSE_DECLTYPES
+		)
+		g.db.row_factory = sqlite3.Row
+
+	return g.db	
+
+def close_db(e=None):
+	db = g.pop('db', None)
+
+	if db is not None:
+		db.close()
+
+def init_db():
+	db = get_db()
+
+	with current_app.open_resource('schema.sql') as f:
+		db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+def init_db_command():
+	init_db()
+	click.echo("initialized database")
+
+sqlite3.register_converter(
+	"timestamp", lambda v: datetime.fromisoformat(v.decode())
+)
+
+
+# DELIMIT OLD CODE AND NEW CODE (OLD BELOW NEW ABOVE)
+
 
 con = sqlite3.connect("suru.db")
 cur = con.cursor()
