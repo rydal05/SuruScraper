c558ade18395521677276db18a234e1b02b8b80d
Ryan <ssj4rydalton@gmail.com>
2026-01-16 00:10:24 -0500

    fixes

--- suru/editor.py
+++ suru/editor.py
@@ -1,10 +1,13 @@
 import functools
 
-from flask import {
+from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
-}
+)
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from suru.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @bp.route('/wishlist', methods=('GET','POST'))
