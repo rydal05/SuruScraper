42d7ec80b2b1b978e962493af92759c16cf37645
Ryan <ssj4rydalton@gmail.com>
2026-05-02 20:50:37 -0400

    a "feature complete" version of suruscraper intended for user desktop use.

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 
 waitTime = 3.0 # wait in float between webpage accesses
 hourSchedule = "*" # '*' repeats on hourly interval, 0-23 repeat at specific hour of the day
-minuteSchedule = 0 # 0-59 for what time :XX to go on 
+minuteSchedule = 0 # 0-59 for what time :XX to go on
