27b9343bf807f652e9bc1b4321d9afe01b24452c
Ryan Dalton <rdalton2405@gmail.com>
2026-06-23 13:14:35 -0400

    Removed dupe functions

--- suruscrapr/db.py
+++ suruscrapr/db.py
@@ -36,18 +36,6 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-@click.command('init-db')
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
-
 def has_users():
     try:
         return get_db().execute('SELECT 1 FROM user LIMIT 1').fetchone() is not None
@@ -147,27 +135,6 @@ def set_item_status(item_id, in_stock, price, last_checked_at):
 sqlite3.register_converter(
     'timestamp', lambda v: datetime.fromisoformat(v.decode())
 )
-
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
-
-
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
 
 def init_db():
     db = get_db()
