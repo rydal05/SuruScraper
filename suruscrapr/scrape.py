a519baddf0a10d38256aacb71d2ca16d99c2b481
Ryan Dalton <rdalton2405@gmail.com>
2026-06-30 21:56:53 -0400

    more scraping methods & verbose logging

--- suruscrapr/scrape.py
+++ suruscrapr/scrape.py
@@ -44,9 +44,13 @@ def suru_scrape_task(): #TODO: also need to implement cleaner usage
 
 			if not soup: continue # 2: check if pull successful
 			# 2.5: branch depending on which site we're on
-
+			
 			# 3: pull item info from page and propagate database TODO: start pulling high level category (I.e, Video software, Music software, Toy hobby (maybe even trim subcategory or do subsorts))
-			name = updateName(original_name, soup,db,item_id)
+			name = scrape_name(soup)
+			listed_price = scrape_OG_price(soup)
+			current_price = scrape_price(soup)
+			category = scrape_category(soup)
+			media_format = scrape_format(soup)
 
 			# 4: check if item is in stock (split out functions for different sites and whatever)
 			checkIfExists(soup, name,db, item_id)
@@ -60,8 +64,8 @@ def suru_scrape_task(): #TODO: also need to implement cleaner usage
 def updateName(original_name:str, soup:BeautifulSoup, db, item_id): #TODO: should be made cleaner to read
 	name = original_name
 
-	if original_name == "BLANK":
-		title_tag = soup.find("h1", class_="title_product")
+	if original_name == "BLANK": # TODO: change detecting if the item name was blank to utilizing the clean boolean
+		title_tag = soup.find("h1", class_="title_product") # probably also move checking these outside the function in the first place, or use a function to do table value checks
 		if title_tag:
 			name = title_tag.text.strip()
 		for prefix in get_prefixes():
@@ -70,7 +74,7 @@ def updateName(original_name:str, soup:BeautifulSoup, db, item_id): #TODO: shoul
 
 	return name
 
-#Perfect
+# DONE: 
 def getSoup(url:str):
 	response = requests.get(url, timeout=10, headers=headers)
 	if response.status_code != 200:
@@ -83,7 +87,7 @@ def checkIfExists(soup:BeautifulSoup,name:str,db,item_id):
 	addToCartBtn = soup.find("button",id='add-cart-btn') 
 	if addToCartBtn:
 		price_input = soup.find("input", class_="priceValue")
-		price_val = price_input["value"] if price_input else "Unknown"
+		price_val = price_input["value"] if price_input else "Unknown" 
 		concatPrice = f"¥{price_val}"
 		print(f"{name}: AVAILABLE @ {price_val}")
 		curDate = datetime.now().strftime("%m/%d/%Y %H:%M")
@@ -93,3 +97,51 @@ def checkIfExists(soup:BeautifulSoup,name:str,db,item_id):
    )
 	else:
 		print(name + ": PRODUCT UNAVAILABLE")
+
+# DONE:
+def scrape_name(soup:BeautifulSoup):
+	nametag = soup.find("meta", property="og:title")
+	meta_name = nametag.get("content")
+	
+	return meta_name
+
+# FIXME:
+def scrape_format(soup:BeautifulSoup):
+	# 1. Check if media tag is available
+	mediatag = soup.find("span", class_="text-gray-dark text-nowrap") #TODO: needs something more specific to grab at, unreliable
+	next_sib = mediatag.find_next_sibling()
+
+	if next_sib and isinstance(next_sib, str):
+		media_format = next_sib.strip()
+		return media_format
+	
+	return None
+
+	
+# TESTME:
+def scrape_price(soup:BeautifulSoup):
+	addToCartBtn = soup.find("button",id='add-cart-btn') 
+
+	if not addToCartBtn: return None # Item must be in stock to check for price
+
+	price_input = soup.find("input", class_="priceValue")
+	price_val = price_input["value"]
+
+	pass
+
+def scrape_category(soup:BeautifulSoup):
+	category = soup.find("span", class_="text-gray-dark text-nowrap") #TODO: see above
+
+	# skip whitespace
+	# check first category tag (Doujin software, Doujin magazine, etc)
+
+	return None
+
+def scrape_OG_price(soup:BeautifulSoup):
+	# 1. check for label with text "Listed Price:"
+	listed_label = soup.find("label", class_="m1-2 price-suggest")
+	if not listed_label: return None # Item must be in stock to check for price
+	# 2. check following div afterwards
+	# 3. trim "JPY" from the field
+	# 4. return int
+	pass
