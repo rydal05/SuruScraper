0ab7cc02097f78c266185d05a17463ca36e3fdd3
Ryan Dalton <rdalton2405@gmail.com>
2026-07-06 16:21:11 -0400

    with auto managaes proper db closure

--- tests/test_autodb.py
+++ tests/test_autodb.py
@@ -14,19 +14,17 @@ def TEST():
     seed_dbs()
 
 def seed_dbs():
-    for i, seed in enumerate(seed_folder.iterdir()):
-        with open(database_folder/f"test_database{i}.db","w") as file:
-            pass
+    for index, seed in enumerate(seed_folder.iterdir()):
+        # with open(database_folder/f"test_database{i}.db","w") as file:
+        #     pass
 
-        db = database_folder/f"test_database{i}"
+        db_path = database_folder/f"test_database{index}"
 
-        con_db = sqlite3.connect(db)
-        cur_db = con_db.cursor()
+        with sqlite3.connect(db_path) as con_db:
+            cur_db = con_db.cursor()
+
         pages = []
 
-        if not Path(seed).exists():
-            TL.logging.error("seed DNE")
-            return
         
         with open(seed, 'r') as file:
             for line in file:
@@ -39,35 +37,24 @@ def seed_dbs():
         except sqlite3.Error as e:
             TL.logging.error(f'Error occurred while seeding: {e}')
             con_db.rollback()
-        finally:
-            cur_db.close()
-            con_db.close()
 
 def init_dbs():
-    for i in enumerate(seed_folder.iterdir()):
-        with open(database_folder/f"test_database{i}.db","w") as file:
-            pass
-        
-        db = database_folder/f"test_database{i}.db"
+    for index in enumerate(seed_folder.iterdir()):
+        db_path = database_folder/f"test_database{index}.db"
 
-        con_db = sqlite3.connect(db)
-        cur_db = con_db.cursor() 
+        with sqlite3.connect(db_path) as con_db:
+            cur_db = con_db.cursor()
 
-        with open(schema_file, 'r') as f:
-            schema_sql = f.read().strip()
+            with open(schema_file, 'r') as f:
+                schema_sql = f.read().strip()
 
-        try:
-            for statement in schema_sql.split(';'):
-                stripped_statement = statement.strip()
-                if stripped_statement:
-                    cur_db.execute(stripped_statement)
-            
-            con_db.commit()
-        except sqlite3.Error as e:
-            TL.logging.error(f'Error occurred: {e}')
-            con_db.rollback()
+            try:
+                for statement in schema_sql.split(';'):
+                    stripped_statement = statement.strip()
+                    if stripped_statement:
+                        cur_db.execute(stripped_statement)
 
-        finally:
-            cur_db.close()
-            con_db.close()
-            
+                con_db.commit()
+            except sqlite3.Error as e:
+                TL.logging.error(f'Error occurred: {e}')
+                con_db.rollback()
