3fc187be85285ef4ab2ec156309e72f8b765306b
Ryan Dalton <rdalton2405@gmail.com>
2026-07-06 15:18:57 -0400

    proprietary logger configuration

---
+++ suruscrapr/logger.py
@@ -0,0 +1,20 @@
+import logging
+
+def configure_logging():
+    print('configuring')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler('suruscraper.log')
+    file_handler.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
