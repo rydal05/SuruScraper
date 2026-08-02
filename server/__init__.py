f4e138068aada2102269ac99c936f609bdf45d4b
Ryan Dalton <rdalton2405@gmail.com>
2026-08-01 20:40:22 -0400

    removal of auth + fixing other import failures

--- server/__init__.py
+++ server/__init__.py
@@ -5,12 +5,6 @@ logger.info(f'Started in {__name__}')
 from pathlib import Path
 from flask import Flask
 
-import threading
-import asyncio
-
-from server.scheduler import scrapeScheduler
-from server.scrape import suru_scrape_task
-
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
@@ -26,10 +20,8 @@ def create_app(test_config=None):
 
     Path(app.instance_path).mkdir(parents=True, exist_ok=True)
 
-    from . import auth, db, wishlist
-
+    from . import db, wishlist
     db.init_app(app)
-    app.register_blueprint(auth.bp)
     app.register_blueprint(wishlist.bp)
 
     return app
